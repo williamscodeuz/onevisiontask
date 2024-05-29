4f9b24345b6ec85b5b554991aa8c49bd144e709d
williamscode <uz.williamscode@gmail.com>
2024-05-29 21:47:24 +0500

    initial

---
+++ src/main/java/uz/williamscode/onevisiontask/OnevisiontaskApplication.java
@@ -0,0 +1,13 @@
+package uz.williamscode.onevisiontask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnevisiontaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnevisiontaskApplication.class, args);
+	}
+
+}
